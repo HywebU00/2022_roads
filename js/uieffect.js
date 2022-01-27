e2b5e58668a17e058df01b43ab7df7f786d8afd6
yachiung <chiung@hyweb.com.tw>
2022-01-27 12:47:47 +0800

    flow 程式與css完

--- js/uieffect.js
+++ js/uieffect.js
@@ -99,7 +99,10 @@ $(function(){
   }
 
 
-  // -----------------------------------------------------向上捲動箭頭
+
+
+
+  // 向上捲動箭頭 -----------------------------------------------------
 	var _goTop = $('.goTop');
   _goTop.click(function(){
     _html.stop(true,false).animate({scrollTop: 0}, 800);
@@ -115,19 +118,20 @@ $(function(){
 
 
   // ======================================================================
-  //  ----------------------------------------- 點選左右箭頭滑動（非自動輪播）
-  // 寬版顯示三筆，每筆等寬
-  var _flow = $('.flow1');
-  _flow.each(function () {
+  // 點選左右箭頭滑動（非自動輪播 -----------------------------------------
+  // .flow1：寬版顯示三筆，每筆等寬
+  var _flow1 = $('.flow1');
+  _flow1.each(function () {
     let _this = $(this);
     let _floxBox = _this.find('.flowBox');
     let _flowList = _floxBox.find('.flowList');
     let _flowItem = _flowList.children('li');
-    let slideDistance = _flowItem.outerWidth(true);
+    let slideDistance = _flowItem.first().outerWidth(true);
     let slideCount = _flowItem.length;
     let _btnRight = _this.find('.diskBtn.next');
     let _btnLeft = _this.find('.diskBtn.prev');
     const speed = 600;
+    const actClassName = 'active';
     let i = 0;
     let j;
     let _dots = '';
@@ -140,33 +144,43 @@ $(function(){
     }
     _indicator.append(_dots);
     let _indicatItem = _indicator.find('li');
-    _indicatItem.eq(i).addClass('now');
+    _indicatItem.eq(i).addClass(actClassName);
 
     // 依據可視的 slide 項目，決定 indicator 樣式
     indicatReady();
     function indicatReady() {
-      ww = _window.width();
-      _indicatItem.removeClass('now');
-      _indicatItem.eq(i).addClass('now');
-      if (ww < wwMedium && slideCount > 1) {
-        _indicator.show();
+      _indicatItem.removeClass(actClassName);
+      _indicatItem.eq(i).addClass(actClassName);
+      if (ww < wwMedium) {
+        if (slideCount > 1) {
+          flownavShow();
+        } else {
+          flownavHide();
+        }
       }
       if (ww >= wwMedium) {
         if (slideCount <= 2) {
-          _indicator.hide();
+          flownavHide();
         } else {
-          _indicator.show();
-          _indicatItem.eq((i + 1) % slideCount).addClass("now");
+          flownavShow();
+          _indicatItem.eq((i + 1) % slideCount).addClass(actClassName);
         }
       }
       if (ww >= wwNormal) {
         if (slideCount <= 3) {
-          _indicator.add(_btnRight).add(_btnLeft).hide();
+          flownavHide();
         } else {
-          _indicatItem.eq((i + 1) % slideCount).addClass("now");
-          _indicatItem.eq((i + 2) % slideCount).addClass("now");
+          flownavShow();
+          _indicatItem.eq((i + 1) % slideCount).addClass(actClassName);
+          _indicatItem.eq((i + 2) % slideCount).addClass(actClassName);
         }
       }
+    }
+    function flownavShow(){
+      _indicator.add(_btnRight).add(_btnLeft).show();
+    }
+    function flownavHide(){
+      _indicator.add(_btnRight).add(_btnLeft).hide();
     }
 
     function slideForward() {
@@ -174,16 +188,15 @@ $(function(){
         'margin-left': -1 * slideDistance }, speed, function () {
           j = (i + 1) % slideCount;
           _flowItem.eq(i).appendTo(_flowList);
-          _indicatItem.eq(i).removeClass('now');
-          _indicatItem.eq(j).addClass('now');
+          _indicatItem.eq(i).removeClass(actClassName);
+          _indicatItem.eq(j).addClass(actClassName);
           _flowList.css('margin-left', 0);
-
-          // if (ww >= wwMedium) {
-          //   _indicatItem.eq((j + 1) % slideCount).addClass('now');
-          // }
+          if (ww >= wwMedium) {
+            _indicatItem.eq((j + 1) % slideCount).addClass(actClassName);
+          }
           if (ww >= wwNormal) {
-            _indicatItem.eq((j + 1) % slideCount).addClass('now');
-            _indicatItem.eq((j + 2) % slideCount).addClass('now');
+            _indicatItem.eq((j + 1) % slideCount).addClass(actClassName);
+            _indicatItem.eq((j + 2) % slideCount).addClass(actClassName);
           }
           i = j;
         });
@@ -194,27 +207,23 @@ $(function(){
       _flowList.css("margin-left", -1 * slideDistance);
 
       _flowList.stop(true, false).animate({ "margin-left": 0 }, speed, function () {
-          _indicatItem.eq(j).addClass("now");
+          _indicatItem.eq(j).addClass(actClassName);
           if (ww >= wwNormal) {
-            _indicatItem.eq((i + 2) % slideCount).removeClass("now");
-          // } else if (ww >= wwMedium) {
-          //   _indicatItem.eq((i + 1) % slideCount).removeClass("now");
+            _indicatItem.eq((i + 2) % slideCount).removeClass(actClassName);
+          } else if (ww >= wwMedium) {
+            _indicatItem.eq((i + 1) % slideCount).removeClass(actClassName);
           } else {
-            _indicatItem.eq(i).removeClass("now");
+            _indicatItem.eq(i).removeClass(actClassName);
           }
           i = j;
         });
     }
 
     // 點擊向右箭頭
-    _btnRight.click(function () {
-      slideForward();
-    });
+    _btnRight.click(function () { slideForward(); });
 
     // 點擊向左箭頭
-    _btnLeft.click(function () {
-      slideBackward();
-    });
+    _btnLeft.click(function () { slideBackward(); });
 
     // touch and swipe 左右滑動
     _floxBox.swipe({
@@ -223,20 +232,20 @@ $(function(){
       threshold: 20,
     });
 
-    var winResizeTimer2;
+    let winResizeTimer;
     _window.resize(function () {
-      clearTimeout(winResizeTimer2);
-      winResizeTimer2 = setTimeout(function () {
-        slideDistance = _flowItem.outerWidth(true);
-        _flowList.width(slideDistance * slideCount);
+      clearTimeout(winResizeTimer);
+      winResizeTimer = setTimeout(function () {
+        ww = _window.width();
+        slideDistance = _flowItem.first().outerWidth(true);
         indicatReady();
-      }, 210);
+      }, 200);
     });
-
-
   });
 
-  // 寬版顯示三筆，中間圖放大且與其他li排版不同
+
+
+  // .flow2：寬版顯示三筆，中間圖放大且與其他項排版不同
   var _flowCenter = $('.flow2');
   _flowCenter.each(function () {
     let _this = $(this);
@@ -247,7 +256,8 @@ $(function(){
     let slideCount = _flowItem.length;
     let _btnRight = _this.find('.diskBtn.next');
     let _btnLeft = _this.find('.diskBtn.prev');
-    const speed = 600;
+    const speed = 800;
+    const actClassName = 'active';
     let i = 0;
     let j;
     let _dots = '';
@@ -260,46 +270,41 @@ $(function(){
     }
     _indicator.append(_dots);
     let _indicatItem = _indicator.find('li');
-    _indicatItem.removeClass('active').eq(1).addClass('active');
-    _flowItem.removeClass('active').eq(1).addClass('active');
+    _indicatItem.removeClass(actClassName).eq(1).addClass(actClassName);
+    _flowItem.removeClass(actClassName).eq(1).addClass(actClassName);
 
     function slideForward() {
       j = (i + 1) % slideCount;
-      _flowItem.eq(j).removeClass('active');
-      _flowItem.eq((j + 1) % slideCount).addClass('active');
+      _flowItem.eq(j).removeClass(actClassName);
+      _flowItem.eq((j + 1) % slideCount).addClass(actClassName);
 
       _flowList.stop(true, false).animate({'left': -1 * slideDistance}, speed, function () {
         _flowList.css('left', 0);
         _flowItem.eq(i).appendTo(_flowList);
-        _indicatItem.eq(j).removeClass('active');
-        _indicatItem.eq((j + 1) % slideCount).addClass('active');
+        _indicatItem.eq(j).removeClass(actClassName);
+        _indicatItem.eq((j + 1) % slideCount).addClass(actClassName);
         i = j;
       });
     }
 
     function slideBackward() {
       j = (i - 1) % slideCount;
-      _flowItem.eq((i + 1) % slideCount).removeClass('active');
-      _flowItem.eq(i).addClass('active');
+      _flowItem.eq((i + 1) % slideCount).removeClass(actClassName);
+      _flowItem.eq(i).addClass(actClassName);
       _flowItem.eq(j).prependTo(_flowList);
       _flowList.css('left', -1 * slideDistance);
 
       _flowList.stop(true, false).animate({ 'left': 0  }, speed, function () {
-        // _flowList.css('left', -1 * slideDistance);
-        _indicatItem.removeClass('active').eq(i).addClass('active');
+        _indicatItem.removeClass(actClassName).eq(i).addClass(actClassName);
         i = j;
       });
     }
 
     // 點擊向右箭頭
-    _btnRight.click(function () {
-      slideForward();
-    });
+    _btnRight.click(function () { slideForward(); });
 
     // 點擊向左箭頭
-    _btnLeft.click(function () {
-      slideBackward();
-    });
+    _btnLeft.click(function () { slideBackward(); });
 
     // touch and swipe 左右滑動
     _floxBox.swipe({
@@ -317,7 +322,9 @@ $(function(){
     });
   });
 
-  // 寬版顯示兩筆完整，第三筆顯示局部
+
+
+  // .flow3：寬版顯示兩筆完整，第三筆顯示局部
   var _flow3 = $('.flow3');
   _flow3.each(function () {
     let _this = $(this);
@@ -329,6 +336,7 @@ $(function(){
     let _btnRight = _this.find('.diskBtn.next');
     let _btnLeft = _this.find('.diskBtn.prev');
     const speed = 600;
+    const actClassName = 'active';
     let i = 0;
     let j;
     let _dots = '';
@@ -341,42 +349,52 @@ $(function(){
     }
     _indicator.append(_dots);
     let _indicatItem = _indicator.find('li');
-    _indicatItem.eq(i).addClass('now');
+    _indicatItem.eq(i).addClass(actClassName);
 
     // 依據可視的 slide 項目，決定 indicator 樣式
     indicatReady();
     function indicatReady() {
-      ww = _window.width();
-      _indicatItem.removeClass('now');
-      _indicatItem.eq(i).addClass('now');
-      if (ww < wwMedium && slideCount > 1) {
-        _indicator.show();
-      }
-      if (ww >= wwNormal) {
-        if (slideCount <= 2) {
-          _indicator.add(_btnRight).add(_btnLeft).hide();
+      _indicatItem.removeClass(actClassName);
+      _indicatItem.eq(i).addClass(actClassName);
+      if (ww < wwMedium ) {
+        if ( slideCount > 1) {
+          flownavShow();
         } else {
-          _indicatItem.eq((i + 1) % slideCount).addClass("now");
-          // _indicatItem.eq((i + 2) % slideCount).addClass("now");
+          flownavHide()
+        }
+      }
+      if (ww >= wwMedium) {
+        if (slideCount <= 2) {
+          flownavHide();
+        } else {
+          flownavShow();
+          _indicatItem.eq((i + 1) % slideCount).addClass(actClassName);
         }
       }
     }
+    function flownavShow(){
+      _indicator.add(_btnRight).add(_btnLeft).show();
+    }
+    function flownavHide(){
+      _indicator.add(_btnRight).add(_btnLeft).hide();
+    }
+
 
     function slideForward() {
       _flowList.stop(true, false).animate({ 
         'margin-left': -1 * slideDistance }, speed, function () {
           j = (i + 1) % slideCount;
           _flowItem.eq(i).appendTo(_flowList);
-          _indicatItem.eq(i).removeClass('now');
-          _indicatItem.eq(j).addClass('now');
+          _indicatItem.eq(i).removeClass(actClassName);
+          _indicatItem.eq(j).addClass(actClassName);
           _flowList.css('margin-left', 0);
 
           // if (ww >= wwMedium) {
-          //   _indicatItem.eq((j + 1) % slideCount).addClass('now');
+          //   _indicatItem.eq((j + 1) % slideCount).addClass(actClassName);
           // }
           if (ww >= wwNormal) {
-            _indicatItem.eq((j + 1) % slideCount).addClass('now');
-            // _indicatItem.eq((j + 2) % slideCount).addClass('now');
+            _indicatItem.eq((j + 1) % slideCount).addClass(actClassName);
+            // _indicatItem.eq((j + 2) % slideCount).addClass(actClassName);
           }
           i = j;
         });
@@ -384,31 +402,27 @@ $(function(){
     function slideBackward() {
       j = (i - 1) % slideCount;
       _flowItem.eq(j).prependTo(_flowList);
-      _flowList.css("margin-left", -1 * slideDistance);
+      _flowList.css('margin-left', -1 * slideDistance);
 
-      _flowList.stop(true, false).animate({ "margin-left": 0 }, speed, function () {
-          _indicatItem.eq(j).addClass("now");
+      _flowList.stop(true, false).animate({ 'margin-left': 0 }, speed, function () {
+          _indicatItem.eq(j).addClass(actClassName);
           if (ww >= wwNormal) {
-            // _indicatItem.eq(i).removeClass("now");
-            _indicatItem.eq((i + 1) % slideCount).removeClass("now");
+            // _indicatItem.eq(i).removeClass(actClassName);
+            _indicatItem.eq((i + 1) % slideCount).removeClass(actClassName);
           // } else if (ww >= wwMedium) {
-          //   _indicatItem.eq((i + 1) % slideCount).removeClass("now");
+          //   _indicatItem.eq((i + 1) % slideCount).removeClass(actClassName);
           } else {
-            _indicatItem.eq(i).removeClass("now");
+            _indicatItem.eq(i).removeClass(actClassName);
           }
           i = j;
         });
     }
 
     // 點擊向右箭頭
-    _btnRight.click(function () {
-      slideForward();
-    });
+    _btnRight.click(function () { slideForward(); });
 
     // 點擊向左箭頭
-    _btnLeft.click(function () {
-      slideBackward();
-    });
+    _btnLeft.click(function () { slideBackward(); });
 
     // touch and swipe 左右滑動
     _floxBox.swipe({
@@ -421,9 +435,10 @@ $(function(){
     _window.resize(function () {
       clearTimeout(winResizeTimer);
       winResizeTimer = setTimeout(function () {
+        ww = _window.width();
         slideDistance = _flowItem.first().outerWidth(true);
         indicatReady();
-      }, 210);
+      }, 200);
     });
 
 
